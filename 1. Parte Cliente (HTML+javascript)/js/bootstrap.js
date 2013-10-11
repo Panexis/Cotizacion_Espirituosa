96d5d5d67c7bccce1fff4cd2d9b97b26eeea5793
Panexis <bjdonate@gmail.com>
2013-10-11 17:09:26 +0200

    Update bootstrap.js

--- 1. Parte Cliente (HTML+javascript)/js/bootstrap.js
+++ 1. Parte Cliente (HTML+javascript)/js/bootstrap.js
@@ -75,7 +75,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * ========================================================== */
-
+//esto es una mierdaaa
 
 !function ($) {
 
